5075ab961f6d6dd96a65f8b2d4a97e7ee99be369
Hussein, Youssef <youssefmohamed552@gmail.com>
2020-04-27 17:11:31 -0400

    added the CMakeLists.txt

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+ * Author: Youssef Hussein
+ * Project: Connect Four
+ * Topic: Artificial Intelligence
+ */
+
+#include <iostream>
+
+
+int
+main(int argc, char* argv[]){
+  std::cout << "This is Connect Four!! \n";
+  return EXIT_SUCCESS;
+}
